9f5633b1fb76e322e14874ee5e119ec4221c8aa2
Olesia Libra <98489034+Solymar1388@users.noreply.github.com>
2022-09-14 19:33:59 +0300

    deleted some files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
+
+import WeatherSearch from './WeatherSearch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
   </React.StrictMode>
 );
 
